42ced10a0297345dc303f20a390fdb9ecc2a3258
nssglobal <97384712+nssglobal@users.noreply.github.com>
2023-02-11 13:23:17 +0500

    Update models.py

--- work_entry_restrict/models/models.py
+++ work_entry_restrict/models/models.py
@@ -4,6 +4,35 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 from datetime import datetime, timedelta
 
+
+from collections import defaultdict
+from datetime import datetime, date, time
+from dateutil.relativedelta import relativedelta
+import pytz
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.osv import expression
+from odoo.tools import format_date
+
+
+
+class HrPayslipEmployees(models.TransientModel):
+    _inherit = 'hr.payslip.employees'
+
+
+    def _check_undefined_slots(self, work_entries, payslip_run):
+        """
+        Check if a time slot in the contract's calendar is not covered by a work entry
+        """
+        work_entries_by_contract = defaultdict(lambda: self.env['hr.work.entry'])
+        for work_entry in work_entries:
+            work_entries_by_contract[work_entry.contract_id] |= work_entry
+
+        for contract, work_entries in work_entries_by_contract.items():
+            calendar_start = pytz.utc.localize(datetime.combine(max(contract.date_start, payslip_run.date_start), time.min))
+            calendar_end = pytz.utc.localize(datetime.combine(min(contract.date_end or date.max, payslip_run.date_end), time.max))
+            outside = contract.resource_calendar_id._attendance_intervals_batch(calendar_start, calendar_end)[False] - work_entries._to_intervals()
+
 class HrWorkEntryInh(models.TransientModel):
     _inherit = 'hr.work.entry.regeneration.wizard'
 
